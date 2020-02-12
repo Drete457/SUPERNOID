40170d377a4e69cd97e5d147fdd226ee4dcef3ab
Mariana Cavadas <mariana.gcavadas@gmail.com>
2020-02-12 20:56:49 +0000

    created enum with blocks

--- src/SUPERNOID/GameObjects/Block.java
+++ src/SUPERNOID/GameObjects/Block.java
@@ -12,7 +12,6 @@ public class Block {
     private boolean destroyed = false;
     
     public Block(double xPos, double yPos, String url) {
-        setPicture(url);
         picture = new Picture(xPos, yPos, url);
         picture.draw();
     }
@@ -48,6 +47,29 @@ public class Block {
     //Set Picture
     public void setPicture(String url) {
         this.picture.load(url);
+    }
+
+    private enum BlockList {
+        BLUE("SUPERNOID/GameObjects/Images/color-blocks/blue_80x30.jpg"),
+        BROWN("SUPERNOID/GameObjects/Images/color-blocks/brown_80x30.jpg"),
+        GREEN("SUPERNOID/GameObjects/Images/color-blocks/green_80x30.jpg"),
+        CYAN("SUPERNOID/GameObjects/Images/color-blocks/light_blue_80x30.jpg"),
+        LIME("SUPERNOID/GameObjects/Images/color-blocks/light_green_80x30.jpg"),
+        MAGENTA("SUPERNOID/GameObjects/Images/color-blocks/magenta_80x30.jpg"),
+        ORANGE("SUPERNOID/GameObjects/Images/color-blocks/orange_80x30.jpg"),
+        PINK("SUPERNOID/GameObjects/Images/color-blocks/pink_80x30.jpg"),
+        RED("SUPERNOID/GameObjects/Images/color-blocks/red_80x30.jpg"),
+        YELLOW("SUPERNOID/GameObjects/Images/color-blocks/yellow_80x30.jpg");
+
+        private String image;
+
+        BlockList(String image){
+            this.image = image;
+        }
+
+        String getImage() {
+            return this.image;
+        }
     }
 
 }

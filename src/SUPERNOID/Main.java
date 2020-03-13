95635e353971cf9671fffc4b597f6d617fb1bb15
Filipe Mota <drete457@gmail.com>
2020-03-13 01:57:09 +0000

    fix error in the loading of the main screen in jar file

--- src/SUPERNOID/Main.java
+++ src/SUPERNOID/Main.java
@@ -9,52 +9,55 @@ import org.academiadecodigo.simplegraphics.pictures.Picture;
 public class Main {
 
     //The Beginning of the game, and Interrupted when it finishes
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args)  {
 
-        //Create the instruction image
-        Picture firstMenu = new Picture(Grid.PADDING, Grid.PADDING, "Images/general/start_screen.jpg");
+        try {
+            //Create the instruction image
+            Picture firstMenu = new Picture(Grid.PADDING, Grid.PADDING, "resources/Images/general/start_screen.jpg");
 
-        //draw the first menu
-        firstMenu.draw();
+            //draw the first menu
+            firstMenu.draw();
 
-        //print the start time
-        Text startTimer = new Text(firstMenu.getHeight()/2+15,firstMenu.getWidth()/2+100, "5");
+            //print the start time
+            Text startTimer = new Text(firstMenu.getHeight() / 2 + 15, firstMenu.getWidth() / 2 + 100, "5");
 
-        //make the start time white
-        startTimer.setColor(Color.WHITE);
+            //make the start time white
+            startTimer.setColor(Color.WHITE);
 
-        //grow the start time in 25x
-        startTimer.grow(25,25);
+            //grow the start time in 25x
+            startTimer.grow(25, 25);
 
-        //draw the timer
-        startTimer.draw();
+            //draw the timer
+            startTimer.draw();
 
-        //make the timer stay in the screen for 1 second
-        Thread.sleep(1000);
-        startTimer.setText("4");
-        Thread.sleep(1000);
-        startTimer.setText("3");
-        Thread.sleep(1000);
-        startTimer.setText("2");
-        Thread.sleep(1000);
-        startTimer.setText("1");
-        Thread.sleep(1000);
+            //make the timer stay in the screen for 1 second
+            Thread.sleep(1000);
+            startTimer.setText("4");
+            Thread.sleep(1000);
+            startTimer.setText("3");
+            Thread.sleep(1000);
+            startTimer.setText("2");
+            Thread.sleep(1000);
+            startTimer.setText("1");
+            Thread.sleep(1000);
 
-        //delete the firstMenu
-        firstMenu.delete();
+            //delete the firstMenu
+            firstMenu.delete();
 
-        //delete the start timer
-        startTimer.delete();
+            //delete the start timer
+            startTimer.delete();
 
-        //create the object menu
-        Game game = new Game(125);
+            //create the object menu
+            Game game = new Game(125);
 
-        //create the object that will control the game
-        KeyboardManager keyboard = new KeyboardManager(game.getGame(), game.getPaddle(), game.getBall(), game.getEngine());
+            //create the object that will control the game
+            KeyboardManager keyboard = new KeyboardManager(game.getGame(), game.getPaddle(), game.getBall(), game.getEngine());
 
-        //start the game
-        game.start();
-
+            //start the game
+            game.start();
+        }catch (Exception error) {
+            System.out.println("Rebuild the game again plz");
+        }
     }
 }
 
